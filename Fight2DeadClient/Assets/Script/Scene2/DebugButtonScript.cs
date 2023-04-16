e80a484641acb8208b451ab8ec38753b113b1b71
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2023-04-16 17:37:38 +0700

    Debug-Button
    Debug.Log:
    4 button : Group, Options, Online, Solo

---
+++ Fight2DeadClient/Assets/Script/Scene2/DebugButtonScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugButtonScript : MonoBehaviour
+{
+        public void Return()
+    {
+            Debug.Log("Button Pressed");
+    }
+}
