30194eeb5fe7f67957814f19d32d3fb8d6d3ad2e
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2023-04-16 13:08:45 +0700

    test-lan-2
    Running scene 1 first
    menu: scene1, scene2

---
+++ Fight2DeadClient/Assets/Script/Walk.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{   
+    // public Animator animator;
+    // public float LerpConstant;
+    // public float speed;
+    
+    // void Start(){
+    //     rb=GetComponent<Rigibody2D>;
+    // }
+    // void FixedUpdate()
+    // {
+    //     float h = Input.GetAxixRaw("Horizontal");
+    //     Vector2 movement = new Vector2(h, rb.velocity.y);
+    //     rb.velocity=Vector2.Lerp(rb.velocity,speed*movement,LerpConstant);
+
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // if (Input.GetKeyDown(KeyCode.A))
+
+        // {
+        //     animator.SetBool("Walk", true);
+        // }
+        //    if (Input.GetKeyUp(KeyCode.A))
+
+        // {
+        //     animator.SetBool("Walk", false);
+        // }
+    }
+}
