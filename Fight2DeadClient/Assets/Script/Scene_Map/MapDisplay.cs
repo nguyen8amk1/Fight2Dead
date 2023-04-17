d50a57b15e012bd10a532284f4c32e488d2f615f
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2023-04-17 14:22:34 +0700

    Map_Selection(updateVer1)
    Update: Debug.Log on ButtonPlay

--- Fight2DeadClient/Assets/Script/Scene_Map/MapDisplay.cs
+++ Fight2DeadClient/Assets/Script/Scene_Map/MapDisplay.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class MapDisplay : MonoBehaviour
 {
     [SerializeField] private Text mapName;
     [SerializeField] private Image mapImage;
-
+    [SerializeField] private Button playButton;
     public void DisplayMap(Map _newMap)
     {
         mapName.text = _newMap.mapName;
         mapImage.sprite = _newMap.mapImage;
+
+        playButton.onClick.RemoveAllListeners();
+        playButton.onClick.AddListener(()=> Debug.Log(mapName.text));
     }
 }
