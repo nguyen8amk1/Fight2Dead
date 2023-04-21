70fae760b5303518b6b83fbb8763322044bd37c1
nguyen8amk1 <trungnguyen123akohshit@gmail.com>
2023-04-22 01:05:17 +0700

    the animation and the state machine is kinda work now

--- Fight2DeadClient/Assets/Controller.cs
+++ Fight2DeadClient/Assets/Controller.cs
@@ -1,43 +1,138 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    // TODO: create prefabs to automatically spawn 
-    // TODO: find out a way to layer sprite together
+    // STATES 
+    enum GlobalTimingStates
+	{
+        // here we use 1/24 frame time 
+        STATIC,  // 6 frametime 
+        QUAD_IN, // 15 frametime
+        VS_IN,  // 8 frametime
+        EXPLODE,  // 6 frametime
+        TRANSITION // 10 frametime
+	};
+
+    private double staticDuration = ut(6);
+	private double quadInDuration = ut(15); 
+    private double vsInDuration = ut(8);
+	private double explodeDuration = ut(6); 
+	private double transitionDuration = ut(10); 
+
+	private static double ut(int v)
+	{
+        return v*(1.0/24.0);
+	}
+
+    // 4 player
+    private static Vector3 p1Src= new Vector3(12.12f, -4.98f, 0);
+    private static Vector3 p2Src = new Vector3(9.74f, 6.77f, 0);
+    private static Vector3 p3Src = new Vector3(-12.01f, -5.13f, 0);
+    private static Vector3 p4Src = new Vector3(-12.01f, 5.8f, 0);
 
     // Start is called beforeishida the first frame update
-    private Vector3 ishidaDestination = new Vector3(-5.15f, 2.41f, 0f);
-    private Vector3 venomDestination = new Vector3(4.98f, 2.34f, 0);
+    private static Vector3 p1Dest = new Vector3(-5.38f, 2.43f, 0);
+    private static Vector3 p2Dest = new Vector3(-5.38f, -2.37f, 0);
+    private static Vector3 p3Dest = new Vector3(5.39f, 2.38f, 0);
+    private static Vector3 p4Dest = new Vector3(5.39f, -2.31f, 0);
 
-    private Vector3 movementVectorIshida = new Vector3(-5.15f - 11.79f, 2.41f - -4.96f);
-    private Vector3 movementVectorVenom = new Vector3(4.98f - -11.5f, 2.34f - -3.61f);
-    public GameObject ishida;
-    public GameObject venom;
+    // VS 
+    private static Vector3 VSrc = new Vector3(-9.09f, 0.55f, 0);
+    private static Vector3 VDest = new Vector3(-1.8f, 0.1475f, 0);
+
+	private static Vector3 SSrc = new Vector3(10.38f, 0.47f, 0);
+    private static Vector3 SDest = new Vector3(1.95f, 0.11f, 0);
+
+    // VS scale: 
+    private Vector3 VScaleSrc = new Vector3(5.895488f, 5.870328f, 0);
+    private Vector3 SScaleSrc = new Vector3(6.470812f, 6.470812f, 0);
+
+    private Vector3 VScaleDest = new Vector3(1.877777f, 1.869763f, 0);
+    private Vector3 SScaleDest = new Vector3(1.956057f, 1.956057f, 0);
+
+
+    public GameObject p1;
+    public GameObject p2;
+    public GameObject p3;
+    public GameObject p4;
+
+    public GameObject V;
+    public GameObject S;
+
+    private double timingVar = 0;
+
+    private GlobalTimingStates currentState = GlobalTimingStates.STATIC;
 
     void Start()
     {
-        venom.transform.position = new Vector3(-11.5f, -3.61f, 0);
-        ishida.transform.position = new Vector3(11.79f, -4.96f, 0);
+        Application.targetFrameRate = 60;
+
+        p1.transform.position = p1Src;
+        p2.transform.position = p2Src;
+        p3.transform.position = p3Src;
+        p4.transform.position = p4Src;
+
+        V.transform.position = VSrc;
+        S.transform.position = SSrc;
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool ishidaFinish = V3Equal(ishida.transform.position, ishidaDestination);
-        bool venomFinish = V3Equal(venom.transform.position, venomDestination);
-        if(!ishidaFinish)
+        //currentState = nextState(currentState, timingVar);
+        //Debug.Log(currentState);
+        if(currentState == GlobalTimingStates.STATIC)
 		{
-			ishida.transform.position += movementVectorIshida*Time.deltaTime;
+            float t = (float)(timingVar/staticDuration);
+            if(t >= 1) { 
+				timingVar = 0;
+				currentState = GlobalTimingStates.QUAD_IN;
+            }
 		}
-        if(!venomFinish)
+
+        // TODO: lerp the position instead of increase the position incrementally
+        else if(currentState == GlobalTimingStates.QUAD_IN)
 		{
-			venom.transform.position += movementVectorVenom*Time.deltaTime;
+
+            float t = (float)(timingVar/quadInDuration);
+			p1.transform.position = Vector3.Lerp(p1Src, p1Dest, t);
+			p2.transform.position = Vector3.Lerp(p2Src, p2Dest, t);
+			p3.transform.position = Vector3.Lerp(p3Src, p3Dest, t);
+			p4.transform.position = Vector3.Lerp(p4Src, p4Dest, t);
+
+            if(t >= 1) { 
+				timingVar = 0;
+				currentState = GlobalTimingStates.VS_IN;
+            }
 		}
-    }
-    public bool V3Equal(Vector3 a, Vector3 b)
-    {
-        return Vector3.SqrMagnitude(a - b) < 0.0001;
+
+        else if(currentState == GlobalTimingStates.VS_IN)
+		{
+            float t = (float)(timingVar/vsInDuration);
+			// FIXME: overlay problem 
+			V.transform.localScale = Vector3.Lerp(VScaleSrc, VScaleDest, t);
+			V.transform.position = Vector3.Lerp(VSrc, VDest, t);
+
+			S.transform.localScale = Vector3.Lerp(SScaleSrc, SScaleDest, t);
+			S.transform.position = Vector3.Lerp(SSrc, SDest, t);
+
+            if(t >= 1) { 
+				timingVar = 0;
+				currentState = GlobalTimingStates.EXPLODE;
+            }
+		}
+        else if(currentState == GlobalTimingStates.EXPLODE)
+		{
+            // TODO: 
+		}
+        else if(currentState == GlobalTimingStates.TRANSITION)
+		{
+            // TODO: 
+		}
+
+        timingVar += Time.deltaTime;
     }
 }
